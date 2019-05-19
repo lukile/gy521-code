9da60821f0200f0cc2e86723f5e10d353682095c
Lukile <lucile.1988.ls@gmail.com>
2019-05-19 15:22:09 +0200

    Save one accel. struct inside esp

--- gy521-v.2/gy521-v.2.ino
+++ gy521-v.2/gy521-v.2.ino
@@ -39,7 +39,7 @@ void setup(){
   Wire.endTransmission(true);
   EEPROM.begin(512);
 
-    Wire.beginTransmission(MPU_addr);
+  Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
@@ -50,6 +50,10 @@ void setup(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  Serial.println();
+  Serial.println(AcX);
+  Serial.println(AcY);
  
   acc.ax = AcX;
   acc.ay = AcY;
@@ -66,22 +70,10 @@ void setup(){
   Serial.print(" | GyY = "); Serial.print(acc.gy);
   Serial.print(" | GyZ = "); Serial.println(acc.gz);*/
 
-  
-  //Serial.print(" acc.ax : ");Serial.println(acc.ax);
-  //Serial.print(" acc.ay : ");Serial.println(acc.ay);
-
 
   //addr += sizeof(float);
-  //EEPROM.put(addr, acc);
-  //EEPROM.commit();
-
-  //Serial.print(" ax : ");Serial.println(acc.ax);
-  //Serial.print(" ay : ");Serial.println(acc.ay);
-
-
-
-  //clear();
-
+  EEPROM.put(addr, acc);
+  EEPROM.commit();
 
   Accelerometer acc2;
   EEPROM.get(addr, acc2);
@@ -93,41 +85,7 @@ void setup(){
   //clear();
 }
 void loop(){
-  
-
-
-  
-  /*Serial.print("eeprom write anything : ");Serial.print(tracker,DEC);
-  Serial.println();
-  Serial.print("ax value : ");Serial.print(acc.ax,DEC);
-  Serial.println();
-  acc.ax = 0;
-  Serial.println(acc.ax,DEC);
-
-  //t = EEPROM_readAnything(0,acc);
-  Serial.println(t,DEC);
-  
-  
-  
-
-  //EEPROM.write(addr, acc.ax);
-  //addr = addr + 1;
-
-  //if(addr == 512) {erial.print(addr);
-  //Serial.println();
-  //Serial.print(" ax value : "); Serial.print(acc.ax);
-  //Serial.println();
-   // addr = 0;
-    //EEPROM.commit();
-  //}
-  //Serial.print("Ax : ");Serial.println(savedDatas[iterator].ax);
-  //value = EEPROM.read(addr);
-  //Serial.print("address = ");
-  //Serial.print(" value saved : ");Serial.print(value);
-  //Serial.println();
-
   delay(1000);
- // clear();*/
 }
 void clear() {
    for(int i = 0; i < EEPROM.length(); i++) {
