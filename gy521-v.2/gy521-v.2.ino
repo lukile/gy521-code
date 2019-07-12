25a4b1e92d32f0396d9c8ef04564f5623f55bf4b
Lukile <lucile.1988.ls@gmail.com>
2019-07-12 10:34:49 +0200

    Pooling light sensor and compute distance / speed codes

--- gy521-v.2/gy521-v.2.ino
+++ gy521-v.2/gy521-v.2.ino
@@ -23,41 +23,14 @@
 #include <WiFiGeneric.h>
 #include <WiFi.h>
 
-//#define SDA 33
-//#define SCL 32
-
-/*
-TO BEGIN 
-  session date -> compute chronos program at the beginning of session and stock result
-  startTime -> compute chronos program at the beginning of session and stock result
- 
-GET FROM API BEFORE START : 
-  lenghtType (25 or 50)
-  programType (winter, spring, summer Body)
-  user id
-
-SEND TO API AT THE END (update) :
-  speed (computed in this program)
-  distance (computed in this program)
-  endTime -> compute chronos program at the end of session and stock result 
-
-lostWeight?
-
-Define a session and a deviceId in app
- 
-*/
-
-
 MPU6050 mpu6050(Wire);
 
 String device_id = "WDB-0001A";
-int iterator = 0;
 
 long timer = 0;
 long timerStartSession = 0;
 double arraySpeeds[1000];
 int cpt = 0;
-int endSession = 0;
 double currentV = 0;
 double currentX = 0;
 double previousV = 0;
@@ -66,15 +39,13 @@ double previousX = 0;
 double distance = 0;
 double averageSpeed = 0;
 
-String avgSpeed;
-String dist;
+String averageSpeedJson;
+String distanceJson;
 
 float minimalHighAccX = 0.20;
 float minimalLowAccX = -0.20;
 
-String datas;
-
-String dateTime;
+String datePerformance;
 String startTime;
 String endTime;
 
@@ -84,17 +55,11 @@ int lastTimer = 0;
 
 int isPerformanceStart = 0;
 int isWifiStart = 0;
-const char* ssid = "PriseDeLaBastille";
-const char* password = "flow4321";
 
-int addr = 0;
-float savedDatas[3000];
-char jsonTest;
-
-
+const char* ssid = "ssid";
+const char* password = "pass";
 
 int PinSeuilLumiere = 2;   // Broche Numérique mesure d'éclairement
-int tension = 0;            // Mesure de tension (valeur élevée si sombre)
 
 void setup(){
   Serial.begin(9600);
@@ -104,7 +69,7 @@ void setup(){
   mpu6050.calcGyroOffsets(true);
   timerStartSession = millis();
 
-    pinMode(PinSeuilLumiere, INPUT);
+  pinMode(PinSeuilLumiere, INPUT);
 
   
   WiFi.mode(WIFI_STA);
@@ -134,7 +99,6 @@ void loop(){
       firstTimer = millis();
       delay(2000);
   }
-
   
   if(eclaire == 0 && flagEclairage == 1) {
     lastTimer = (millis() - firstTimer) / 1000; // en sec
@@ -149,16 +113,16 @@ void loop(){
         isPerformanceStart = 1;
       } else if(isPerformanceStart == 2) {
         averageSpeed = averageArray(arraySpeeds, cpt);
-        avgSpeed = "\"speed\" : " + (String)averageSpeed + ",";
+        averageSpeedJson = "\"speed\" : " + (String)averageSpeed + ",";
   
         distance = currentV * ((millis() - timerStartSession)/1000);
-        dist = "\"distance\" : " + (String)distance + ",";
+        distanceJson = "\"distance\" : " + (String)distance + ",";
   
         Serial.print("\nvitesse moyenne : ");Serial.print(currentV);Serial.print("m/s\n");
         Serial.print("Distance moyenne : ");Serial.print(distance); Serial.print("m\n");
 
         endTime = "\"endTime\" : \"" + (getCurrentDate() + getCurrentTime()) + "\"";
-        sendToApi(dateTime, startTime, avgSpeed, dist, endTime);
+        sendToApi(datePerformance, startTime, averageSpeedJson, distanceJson, endTime);
         
         sleep(50);
       } 
@@ -180,14 +144,12 @@ void loop(){
 
  
   if(isPerformanceStart == 1) {
-    dateTime = "\"datePerformance\" : \"" +  (getCurrentDate() + getCurrentTime()) + "\"" + ",";
+    datePerformance = "\"datePerformance\" : \"" +  (getCurrentDate() + getCurrentTime()) + "\"" + ",";
     startTime = "\"startTime\" : \"" + (getCurrentDate() + getCurrentTime()) + "\"" + ",";
 
     isPerformanceStart = 2;
   }
   
-  Serial.print("endSession : ");Serial.print(endSession);Serial.print("\n");
-
   if(isPerformanceStart == 2) {
     mpu6050.update();
     if(mpu6050.getAccX() > minimalHighAccX || mpu6050.getAccX() < minimalLowAccX) {
@@ -218,71 +180,14 @@ double averageArray(double *array, int arraySize) {
   return ((double) sum) / arraySize;
 }
 
-String getCurrentUserFromApi() {
-    StaticJsonDocument<600> doc;
-
-  String userIdField = "\"user\" : ";
-
-  if(WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-
-    http.begin("http://192.168.1.18:3000/users");
-
-    
-
-    int httpCode = http.GET();
-    String payload = http.getString();
-
-    DeserializationError error = deserializeJson(doc, payload);
-
-    const char* lengthT = doc["lengthType"];
-
-
-    String completeUserField = userIdField + payload;
-    
-    return completeUserField;
-  }
-}
-
-/*String getBeginningDatasFromApi() {
-  StaticJsonDocument<600> doc;
-
-  if(WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-
-    http.begin("http://192.168.1.18:3000/performances";
-
-    int httpCode = http.GET();
-    String payload = http.getString();
-
-    DynamicJsonDocument doc(1024);
-
-    DeserializationError error = deserializeJson(doc, payload);
-
-    if (error) {
-      Serial.print(error.c_str());
-      return "error";
-    }
-
-    //String lengthType = doc["data"]["performances"][0]["lengthType"];
-    //String programType = doc["data"]["performances"][0]["programType"];
-    ///double session = doc["data"]["performances"][0]["session"];
-    String userId = doc["data"]["performances"][0]["user"];
-
-    datas =  "\"deviceId\" : "+ deviceId + ", \"user\" : " + "\"" + userId + "\"" + ",";
-
-    return datas;
-  }
-}*/
-
-void sendToApi(String dateTime, String startTime, String averageSpeed, String distance, String endTime) {
+void sendToApi(String datePerformance, String startTime, String averageSpeed, String distance, String endTime) {
     HTTPClient http;
 
     //Change
     http.begin("http://192.168.1.18:3000/performances");
     http.addHeader("Content-Type", "application/json");
 
-    String json = "{\"performance\" : { \"deviceId\" : \""+ device_id + "\"" + "," + "\n" + dateTime + "\n" + startTime + "\n" + averageSpeed + "\n" + distance + "\n" + endTime + "\n"+ "}\n}";
+    String json = "{\"performance\" : { \"deviceId\" : \""+ device_id + "\"" + "," + "\n" + datePerformance + "\n" + startTime + "\n" + averageSpeed + "\n" + distance + "\n" + endTime + "\n"+ "}\n}";
 
     Serial.print("json : ");Serial.print(json);
 
@@ -296,8 +201,6 @@ void sendToApi(String dateTime, String startTime, String averageSpeed, String di
     Serial.println(payload);
 
     http.end();
-  
-
  
   delay(1000);
 }
@@ -368,7 +271,6 @@ String getCurrentTime() {
   Serial.println();
 }
 
-
 struct tm* getLocalDateTime() {
   time_t currentTime;
   struct tm *localTime;
@@ -377,5 +279,4 @@ struct tm* getLocalDateTime() {
   localTime= localtime(&currentTime);
 
   return localTime;
-
 }
